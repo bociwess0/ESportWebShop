511f62a907c20a3f7ada54d22332b2c3a78c3482
bociwess0 <bogdandjordjevic2000@gmail.com>
2024-03-10 13:24:58 +0100

    creating react client applictaion

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4328a2d54018e190d57f84fda26beb29f2b40e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4bbba95ccfd8f2a443df47bf137fc23442ed0c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
