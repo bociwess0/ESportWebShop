cdcfbc4128e687bf7179b14e28bd836866c43ed5
bociwess0 <bogdandjordjevic2000@gmail.com>
2024-03-18 22:17:57 +0100

    begining with cart page

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1608e65b105f46ab9d25da0992002dc2663a170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e7f47d7668b66ca742e30c83829c41df4147e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,6 @@ function App() {
 
     getProducts();
     
-    
     dispatch(importProductsFromDatabase({products: products}));
   
   }, [])
