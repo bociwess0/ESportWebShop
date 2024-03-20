c68282f71c8a31a0e941e52dd730812b18f5aadd
bociwess0 <bogdandjordjevic2000@gmail.com>
2024-03-20 21:47:34 +0100

    finishing cart item styling...

--- client/src/Components/Cart/CartItem/CartItem.tsx
+++ client/src/Components/Cart/CartItem/CartItem.tsx
@@ -1,8 +1,29 @@
-
+import productImage from "../../../Assets/headphones.png";
+import classes from './CartItem.module.css';
 
 function CartItem() {
     return(
-        <div className="cartItem"></div>
+        <div className={classes.cartItem}>
+            <div className={classes.imgAndPrice}>
+                <div className={classes.imgWrapper}>
+                    <img src={productImage} alt="" />
+                </div>
+                <div className={classes.cartInfo}>
+                    <div className={classes.category}>MICE</div>
+                    <div className={classes.name}>MAD CATZ R.A.T.1</div>
+                </div>
+            </div>
+            <div className={classes.quantityWrapper}>
+                <div className={classes.quantityText}>Quantity:</div>
+                <button className={classes.minus}>—</button>
+                <input className={classes.number} value={1} />
+                <button className={classes.plus}>+</button>
+            </div>
+            <div className={classes.price}>20000€</div>
+            <div className={classes.btnWrapper}>
+                <button>Remove</button>
+            </div>
+        </div>
     )
 }
 
--- client/src/Components/Cart/CartProducts/CartProducts.tsx
+++ client/src/Components/Cart/CartProducts/CartProducts.tsx
@@ -1,8 +1,12 @@
+import { Container } from "react-bootstrap";
+import CartItem from "../CartItem/CartItem";
 
 function CartProducts() {
     return (
         <div className="cartProducts">
-
+            <Container>
+                <CartItem />
+            </Container>
         </div>
     )
 }
