488d632d351ee2ecf57f8d9fd504ca00003b32e8
bociwess0 <bogdandjordjevic2000@gmail.com>
2024-04-01 21:39:33 +0200

    starting with cart page in steps

--- client/src/Pages/Cart.tsx
+++ client/src/Pages/Cart.tsx
@@ -1,10 +1,9 @@
+import { Outlet } from "react-router-dom";
 import CartProducts from "../Components/Cart/CartProducts/CartProducts";
 
 function Cart() {
     return(
-        <div>
-            <CartProducts />
-        </div>
+        <Outlet />
     )
 }
 
--- client/src/Router/Routes.tsx
+++ client/src/Router/Routes.tsx
@@ -2,14 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Cart from "../Pages/Cart";
 import App from "../App";
 import Products from "../Pages/Products";
+import CartProducts from "../Components/Cart/CartProducts/CartProducts";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
-            {path: 'products', element: <Products />},
-            {path: 'cart', element: <Cart />}
+            { path: 'products', element: <Products /> },
+            { path: 'cart', element: <CartProducts />, children: [
+                {
+                    path: 'address', 
+                    element: <CartProducts />
+                }
+            ]}
         ]
     }
 ])
