4acbaf0fae3d82d7ff4f87886f501e364a3376d9
bociwess0 <bogdandjordjevic2000@gmail.com>
2024-04-09 21:14:27 +0200

    starting with checkout info step

---
+++ client/src/Components/Cart/CheckoutInfo/CheckoutItems/CheckoutItem/CheckoutItem.tsx
@@ -0,0 +1,28 @@
+
+import classes from './CheckoutItem.module.css';
+import productImage from "../../../../../Assets/headphones.webp";
+import { Product } from '../../../../../Interfaces/Interface';
+
+
+interface Props {
+    product: Product
+}
+
+function CheckoutItem({product} : Props) {
+    return(
+        <div className={classes.cartItem}>
+            <div className={classes.imgAndPrice}>
+                <div className={classes.imgWrapper}>
+                    <img src={productImage} alt="" />
+                </div>
+                <div className={classes.checkoutInfo}>
+                    <div className={classes.category}>{product.brand}</div>
+                    <div className={classes.name}>{product.name}</div>
+                </div>
+            </div>
+            <div className={classes.price}>{`${product.price}€`}</div>
+        </div>
+    )
+}
+
+export default CheckoutItem;
---
+++ client/src/Components/Cart/CheckoutInfo/CheckoutItems/CheckoutItems.tsx
@@ -0,0 +1,6 @@
+
+function CheckoutItems() {
+
+}
+
+export default CheckoutItems;
